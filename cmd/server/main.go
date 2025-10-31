bcc4472b893d0115668e91701ae9a97da1b9941a
Kuber <kuberkhandare@gmail.com>
2025-10-31 14:14:18 +0530

    schedule jobs v01

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	// "time"
+
 	"github.com/KuberTheGreat/Sentrinet/internal/api"
 	"github.com/KuberTheGreat/Sentrinet/internal/db"
+	// "github.com/KuberTheGreat/Sentrinet/internal/scheduler"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,6 +14,14 @@ func main(){
 	app := fiber.New()
 
 	api.SetupRoutes(app, database)
+
+	// job := scheduler.Job{
+	// 	Target: "scanme.nmap.org",
+	// 	StartPort: 20,
+	// 	EndPort: 100,
+	// 	Interval: 1 * time.Minute,
+	// }
+	// scheduler.StartJob(database, job)
 
 	app.Listen(":8080")
 }
---
+++ internal/scheduler/scheduler.go
@@ -0,0 +1,43 @@
+package scheduler
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/KuberTheGreat/Sentrinet/internal/scan"
+	"github.com/jmoiron/sqlx"
+)
+
+type Job struct{
+	Target string
+	StartPort int
+	EndPort int
+	Interval time.Duration
+}
+
+func StartJob(db *sqlx.DB, job Job){
+	ticker := time.NewTicker(job.Interval)
+	go func(){
+		for{
+			<-ticker.C
+			fmt.Printf("[Scheduler] Running recurring scan for %s\n", job.Target)
+
+			results := scan.ScanRange(job.Target, job.StartPort, job.EndPort)
+			for _, r := range results{
+				_, err := db.NamedExec(
+					`INSERT INTO scans (target, port, is_open, duration_ms)
+					VALUES (:target, :port, :is_open, :duration_ms)`,
+					map[string]interface{}{
+						"target": job.Target,
+						"port": r.Port,
+						"is_open": r.IsOpen,
+						"duration_ms": r.Duration,
+					},
+				)
+				if err != nil{
+					fmt.Println("Recurring insert error: ", err)
+				}
+			}
+		}
+	}()
+}
